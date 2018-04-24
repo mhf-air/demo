592a4ce1e1972e0a1027cfa6ef5624e4c4f8b2fc
mhf <mhfcbz@163.com>
2018-04-24 10:54:43 +0800

    s:

--- src/root/main/a.vue
+++ src/root/main/a.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 div.root.g-v.j-c-center
-  mt-button(type="primary" @click="getLocation") 获取位置
+  mt-button.button(type="primary" @click="getLocation") 获取位置
+  mt-button.button(type="danger" @click="getPicture") 获取图片
+  img(:src="imgSrc" v-if="imgSrc !== ''")
 </template>
 
 <script>
@@ -9,12 +11,11 @@ import { Toast } from "mint-ui"
 export default {
   data(){
     return {
-      content: "hello",
+      imgSrc: "",
     }
   },
   methods: {
     getLocation(){
-      let self = this
       function onSuccess(pos) {
         Toast(`
           Latitude: ${pos.coords.latitude},
@@ -32,6 +33,25 @@ export default {
       navigator.geolocation.getCurrentPosition(onSuccess, onError, {
         timeout: 3000,
         enableHighAccuracy: true,
+      })
+    },
+
+    getPicture(){
+      let self = this
+      function onSuccess(imageUri) {
+        Toast(imageUri)
+        self.imgSrc = imageUri
+      }
+
+      function onError(error) {
+        Toast(`Failed because: ${error}`)
+      }
+
+      navigator.camera.getPicture(onSuccess, onError, {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // sourceType: Camera.PictureSourceType.CAMERA,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
       })
     },
   },
@@ -54,5 +74,8 @@ export default {
 
 .root
   margin: 3rem 3rem
+
+.button
+  margin-top: 1rem
 
 </style>
